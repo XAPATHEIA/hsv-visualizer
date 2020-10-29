bd1852d67c8985229bf02ec8fd977e94e5efc5bd
XAPATHEIA <54541033+XAPATHEIA@users.noreply.github.com>
2020-10-29 16:50:20 +0000

    build from array progress

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from PIL import Image as im
+import colorsys
 import cv2
-from cv2 import error
+from PIL import Image as im
+import numpy as np
 
 
 # Check for valid file location, read using cv2
 def img_init():
-    try:
-        img = cv2.imread(input("Enter the path of the image you would like to convert to HSV: "))
-        return img
-    except:
-        print("Incorrect Path")
+    return cv2.imread(input("Enter the path of the image you would like to convert to HSV: "))
 
 
 base_img = img_init()
@@ -18,36 +15,38 @@ base_img = img_init()
 # User interface
 def ui():
     try:
-        choice = input("HSV) Edit HSV bands\nRGB) Convert RGB to HSV\n")
+        choice = input("(E)dit HSV bands\n(C)onvert RGB image to HSV image\n").lower()
         return choice
     except ValueError as E:
         print(E)
 
-
+        
 # HSV Customization
 def hsv_adjust():
     hsv_bands = ('HUE', 'SATURATION', 'VALUE')
-    altered_bands = []
-    for channel in hsv_bands:
-        try:
-            altered_bands.append(int(input(f"{channel} (0-3): ")))
-        except ValueError as E:
-            print(E)
-    return altered_bands
+    selection = input("(H)ue\n(S)aturation\n(V)alue\n(A)ll\nSelection: ").lower()
+    if selection == 'a':
+        altered_bands = []
+        for channel in hsv_bands:
+            if channel == hsv_bands[0]:
+                altered_bands.append((int(input(f"{channel} (0-360): "))))
+        return altered_bands
+    else:
+        return (selection, (int(input(f"{channel} (0-100): "))))
+
 
 
 # Constructing HSV image
 def construction(c, image):
-    if c == 'RGB':
+    if c == 'c':
         hsv_img = cv2.cvtColor(base_img, cv2.COLOR_BGR2HSV)
         return hsv_img
-    if c == 'HSV':
+    if c == 'e':
         try:
             hsv = hsv_adjust()
-            new_hsv = image[:,:hsv[2]]
-            return new_hsv
         except IndexError:
             print("Inputted HSV Band Integer out of range")
+
 
 
 # Displaying the image
