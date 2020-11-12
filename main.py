155988dcc01fd6562c8d07dd3a8bdc7dbb0aa928
XAPATHEIA <54541033+XAPATHEIA@users.noreply.github.com>
2020-11-12 18:10:38 +0000

    not really working

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from PIL import Image as im
 import numpy as np
 
 
-# TODO: Might need to change img_init() to return a PIL image object instead of cv2 
-#       so that I can read the pixel data and then construct using array.
+hsv_bands = ('hue', 'saturation', 'value')
+
 
 # Check for valid file location, read using cv2
 def img_init():
@@ -26,39 +26,42 @@ def ui():
         
 # HSV Customization
 def hsv_adjust():
-    hsv_bands = ('hue', 'saturation', 'value')
     selection = input("Hue\nSaturation\nValue\nAll\nSelection: ").lower()
     if selection == 'all':
         altered_bands = []
         for channel in hsv_bands:
-            if channel == hsv_bands[0]:
+            if channel == 'hue':
                 altered_bands.append((int(input(f"{channel.upper()} (0-360): "))))
             else:
                 altered_bands.append((int(input(f"{channel.upper()} (0-100): "))))
-        return altered_bands
+        return selection, altered_bands
     else:
-        return (selection, (int(input(f"{channel} (0-100): "))))
+        if selection == 'hue':
+            return (selection, (int(input(f"{selection} (0-360): "))))
+        else:
+            return (selection, (int(input(f"{selection} (0-100): "))))
 
 
 
 # Constructing HSV image
 def construction(c, image):
     if c == 'c':
-        hsv_img = cv2.cvtColor(base_img, cv2.COLOR_BGR2HSV)
+        hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         return hsv_img
     if c == 'e':
-        hsv = hsv_adjust()
-        if len(hsv) == 3:
-            pass
+        selection, altered_bands = hsv_adjust()
+        if selection == 'all':
+            for band, alteration in enumerate(altered_bands):
+                image[:,:,band] = alteration
+            return image
         else:
-            pass
-
-
+            h_s_v = hsv_bands.index(selection)
+            image[:,:,h_s_v] = altered_bands
+            return image
 
 
 # Displaying the image
 def image_display(final):
-    cv2.imshow('Original Image', base_img)
     cv2.imshow('HSV image', final)
 
     # Wait for input to close
